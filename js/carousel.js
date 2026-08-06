b0dc9805403c3fea07970937156b51a42bb81187
devbrunosfr <sr.brunosfr@gmail.com>
2026-08-06 04:48:12 -0300

    v1.0

--- js/carousel.js
+++ js/carousel.js
@@ -15,34 +15,34 @@ class Carousel {
         this.Url = url;
     }
 
-    
-      
-    static Start(arr){
-        if(arr){
-            if(arr.length > 0){
-                Carousel._sequence = 0;
-                Carousel._size = arr.length;
-                Carousel._arr = arr;
-                Carousel.Next(); //start
-                Carousel._interval = setInterval(function(){ 
-                    Carousel.Next(); 
-                },2000);
-            }
+    static Start(arr) {
+        if (arr && arr.length > 0) {
+            Carousel._sequence = 0;
+            Carousel._size = arr.length;
+            Carousel._arr = arr;
+            Carousel.Next(); 
+            Carousel.ResetTimer();
+            Carousel.InitButtons(); 
         } else {
             throw "Method Start need a Array Variable.";
         }
     }
 
-    static Next(){
+    static ResetTimer() {
+        clearInterval(Carousel._interval);
+        Carousel._interval = setInterval(function () {
+            Carousel.Next();
+        }, 2000);
+    }
+
+    static Next() {
         if (!Carousel._arr || Carousel._size === 0) return;
 
         let imagemAtual = Carousel._arr[Carousel._sequence];
-
         let imagemSeguimento = document.getElementById("carousel");
-        let imagemTitulo = document.getElementById("carousel-title")
+        let imagemTitulo = document.getElementById("carousel-title");
 
         if (imagemSeguimento && imagemTitulo) {
-            
             imagemSeguimento.innerHTML = `
             <a href="${imagemAtual.Url}">
                 <img src="${imagemAtual.Image}"></a>`;
@@ -56,7 +56,34 @@ class Carousel {
                 imagemSeguimento.style.opacity = "1";
             }, 1);
         }
+
         Carousel._sequence = (Carousel._sequence + 1) % Carousel._size;
     }
 
-};
+    static Prev() {
+        if (!Carousel._arr || Carousel._size === 0) return;
+
+        
+        Carousel._sequence = (Carousel._sequence - 2 + Carousel._size) % Carousel._size;
+
+        Carousel.Next();
+        Carousel.ResetTimer();
+    }
+
+    // Vincula o clique das classes .btesquerda e .btdireita
+    static InitButtons() {
+        const btnEsquerda = document.querySelector('.btesquerda');
+        const btnDireita = document.querySelector('.btdireita');
+
+        if (btnEsquerda) {
+            btnEsquerda.addEventListener('click', () => Carousel.Prev());
+        }
+
+        if (btnDireita) {
+            btnDireita.addEventListener('click', () => {
+                Carousel.Next();
+                Carousel.ResetTimer();
+            });
+        }
+    }
+}
