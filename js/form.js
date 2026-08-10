619386b5eced8b7c4042efe9a1f8c3763ae8b362
devbrunosfr <sr.brunosfr@gmail.com>
2026-08-09 22:49:05 -0300

    Adição de usuario ao console.log

--- js/form.js
+++ js/form.js
@@ -2,26 +2,44 @@
 //class contato
 
 class contato {
-    
+    constructor(nome, sobrenome, email, cpf, telefone, tipoContato) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.tipoContato = tipoContato;
+    }
 }
 
 function Post(form) {
+    
+    let data = new contato(
+        form.elements.namedItem("nome").value,
+        form.elements.namedItem("sobrenome").value,
+        form.elements.namedItem("email").value,
+        form.elements.namedItem("cpf").value,
+        form.elements.namedItem("telefone").value,
+        form.elements.namedItem("contato").value
+    );
 
-  let data = new contato(form.elements.namedItem("nome").value,
-            form.elements.namedItem("sobrenome").value, 
-            form.elements.namedItem("email").value, 
-            form.elements.namedItem("cpf").value, 
-            form.elements.namedItem("telefone").value, 
-            form.elements.namedItem("contato").value);
-  
-}
+    console.log("Dados do Contato:", data);
 
-function Enviar() {
-
-    var nome = document.getElementById("nomeid");
-
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    return data;
     }
 
+    function Enviar (event, form) {
+    
+        event.preventDefault();
+        var nome = form.elements.namedItem("nome");
+
+    if(nome.value !== "") {
+
+        let data = Post(form); 
+        
+
+        alert('Obrigado sr(a) ' + data.nome + ', os seus dados foram encaminhados com sucesso');
+    } else {
+        alert('Por favor, preencha o campo em branco.');
+    }
 }
